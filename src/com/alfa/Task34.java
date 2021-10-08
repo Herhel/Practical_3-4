4a8f71f7920243bcc547b9ef79446e6de2331f44
Herhel <herhelqa@gmail.com>
2021-10-08 16:12:43 +0300

    Work_4-3

--- src/com/alfa/Task34.java
+++ src/com/alfa/Task34.java
@@ -1,6 +1,6 @@
 package com.alfa;
 
-import com.alfa.work2.Runner;
+import com.alfa.work3.Runner;
 
 public class Task34 {
     public static void main(String[] args) {
---
+++ src/com/alfa/work3/Runner.java
@@ -0,0 +1,42 @@
+package com.alfa.work3;
+
+public class Runner {
+    public void run() {
+        Storage storage = new Storage();
+
+        Thread printer = new Thread(() -> {
+            synchronized (storage) {
+                while (!Thread.interrupted()) {
+                    while (!storage.isFlag()) {
+                        try {
+                            storage.wait();
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                    System.out.println(storage.getValue());
+                    storage.setFlag(false);
+                    storage.notify();
+                }
+            }
+        });
+
+        printer.start();
+
+        new Thread(() -> {
+            synchronized (storage) {
+                for (int i = 0; i < 20; i++) {
+                    while (storage.isFlag()) {
+                        try {
+                            storage.wait();
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                    storage.setValue(storage.getValue() + 1);
+                    storage.setFlag(true);
+                    storage.notify();
+                }
+                printer.interrupt();
+            }
+        }).start();
+    }
+}
