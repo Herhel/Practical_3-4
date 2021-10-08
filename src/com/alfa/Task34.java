6227d2221a7641c0d030552fd608654ebdc82e75
Herhel <herhelqa@gmail.com>
2021-10-08 14:44:54 +0300

    Work_4-1.1

--- src/com/alfa/Task34.java
+++ src/com/alfa/Task34.java
@@ -3,7 +3,6 @@ package com.alfa;
 import com.alfa.work1.Runner;
 
 public class Task34 {
-
     public static void main(String[] args) {
 	new Runner().run();
     }
--- src/com/alfa/work1/Runner.java
+++ src/com/alfa/work1/Runner.java
@@ -2,6 +2,10 @@ package com.alfa.work1;
 
 public class Runner {
     public void run() {
+        new MyTimeBomb_1().start();
+        new Thread(new MyTimeBomb_2()).start();
+        System.out.println("main ended");
+
         new Thread( () -> {
             for (int i = 10; i > 0 ; i--) {
                 System.out.println(i);
